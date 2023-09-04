445204f46e72a76bd9f23b3f24be802f3da77436
Nagendra07-moh <nagedra.mohan.5@gmail.com>
2023-09-04 22:10:02 +0530

    code

---
+++ new.js
@@ -0,0 +1,7 @@
+document.querySelector('#category').addEventListener('click', (e)=>{
+    // console.log("category parent clicked!!");
+
+    let prodSelected = e.target.id;
+    console.log(prodSelected);
+
+})
