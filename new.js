5f3b7849a6869b48c2a73cbad7c79ed15cc55ece
Nagendra07-moh <nagedra.mohan.5@gmail.com>
2023-09-05 11:39:07 +0530

    fdd

--- new.js
+++ new.js
@@ -4,4 +4,21 @@ document.querySelector('#category').addEventListener('click', (e)=>{
     let prodSelected = e.target.id;
     console.log(prodSelected);
 
+    if(e.target.tagName == 'LI'){
+        
+        window.location.href = '/' + prodSelected //to redirect to the new 
+    }
+
+})
+
+//FOR THE FROMS (convert it to uppercase)
+
+document.querySelector('#form').addEventListener('keydown', (e)=>{
+    // console.log("category parent clicked!!");
+    console.log(e);
+
+    if(e.target.dataset.conupper != undefined){
+        e.target.value = e.target.value.toUpperCase();
+    }
+
 })
