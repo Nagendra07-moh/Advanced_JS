b80f40b3b572e0b054c6848a09bfcd6f51991f9f
Nagendra07-moh <nagedra.mohan.5@gmail.com>
2023-09-09 21:42:45 +0530

    hola

--- Promises/new.js
+++ Promises/new.js
@@ -4,3 +4,17 @@ function getData(){
     //calls an api  to fetch the data
     console.log("Fetching data...",counter++);
 }
+
+const doSomeMagic = function(fn , d){
+    let timer;
+    let context = this;
+        args = arguments;
+    clearTimeout(timer);    
+    return function(){
+        timer = setTimeout(()=>{
+            getData.apply(context,arguments);
+        },d)
+    }
+}
+
+const betterFunction = doSomeMagic(getData,3000);
